9a8cbc4e537762ced9f421aa7a7e8a5e406ba288
Tony Lee <var.tonylee@gmail.com>
2019-09-27 23:13:55 -0400

    Update wxParseInnerAudio.vue
    Fix issue for mini app of wechat

--- src/components/wxParseInnerAudio.vue
+++ src/components/wxParseInnerAudio.vue
@@ -1,12 +1,9 @@
 <template>
-
     <div class="record_box">
-
         <div @click='playRecord' class='record_btns'>
             <i v-if="isPaused" class="icon icon-play"></i>
             <i v-else class="icon icon-pause"></i>
         </div>
-
         <div class='record_actions pt-1' v-show="isSeekBtn">
             <span class="record_speed mr-1" @click="backwardMusic(10)">
                 <img mode="widthFix" style="width: 35px;" src="/static/images/10_sec_backward.png">
@@ -15,7 +12,6 @@
                 <img mode="widthFix" style="width: 35px;" src="/static/images/10_sec_forward.png">
             </span>
         </div>
-
         <div class='record_wave' :class=" isPaused ? '': 'active'">
             <div class='wave1'></div>
             <div class='wave2'></div>
@@ -28,59 +24,56 @@
             <div class='wave9'></div>
             <div class='wave10'></div>
         </div>
-
         <div class="features">
             <div class='record_time'>
                 <span>{{lastTime}}</span>
             </div>
+            <!--            <div class='record_actions'>-->
+            <!--                <van-button custom-class="record_speed" color="#097C25" hairline plain size="mini">- 10</van-button>-->
+            <!--                <van-button custom-class="record_speed" color="#097C25" hairline plain size="mini">10 +</van-button>-->
+            <!--            </div>-->
         </div>
-
         <slot></slot>
     </div>
 </template>
-
 <script>
-    export default {
-        name    : 'audio',
-        props   : {
-            url: {
-                type : String,
-                value: ''
-            },
-            seek:{
-                type : Boolean,
-                value : ''
-            }
-        },
-        data() {
-            return {
+	export default {
+		name    : 'wxParseInnerAudio',
+		props   : {
+			node: {
+				type: Object,
+				default() {
+					return {}
+				}
+			}
+		},
+		data() {
+			return {
                 innerAudio       : {},
                 isPaused         : true, //是否暂停,true 暂停
                 lastTime         : '00:00"', //剩余时间
                 currentTime      : 0, //播放时间
                 clearWaveInterval: null, //其他音频播放的时候暂停动画,
                 clearTimeTrigger : null, //其他音频播放的时候暂停动画,
-                isSeekBtn        : this.seek === '' ? false : this.seek
-            }
-        },
-        onLoad( options ) {
-            console.log( this.isSeekBtn, this.seek )
-            this.innerAudio = wx.createInnerAudioContext()
-            this.innerAudio.onEnded( () => {
-                this.innerAudio.offTimeUpdate()
-                this.isPaused    = true
-                this.currentTime = 0
-            } )
-            this.innerAudio.onPlay( () => {
-                this.isPaused = false
-                if( this.innerAudio.currentTime === 0 ) {
-                    this.clearTimeTrigger = setTimeout( () => {
-                        console.log( 'currentTime ：', this.innerAudio.currentTime )
-                    }, 500 )
-                }else{
-                    clearTimeout( this.clearTimeTrigger )
-                }
+                isSeekBtn        : this.node.attr.seek
+			}
+		},
+		onLoad( options ) {
+			this.innerAudio = wx.createInnerAudioContext()
+			this.innerAudio.onEnded( () => {
+				this.innerAudio.offTimeUpdate()
+				this.isPaused    = true
+				this.currentTime = 0
+			} )
 
+			this.innerAudio.onPlay( () => {
+				if( this.innerAudio.currentTime === 0 ) {
+					this.clearTimeTrigger = setTimeout( () => {
+						console.log( 'currentTime ：', this.innerAudio.currentTime )
+					}, 500 )
+				}else{
+					clearTimeout( this.clearTimeTrigger )
+				}
                 this.innerAudio.onTimeUpdate( () => {
                     this.currentTime = this.innerAudio.currentTime
                     this.lastTime    = this.formatSeconds( this.currentTime )
@@ -90,29 +83,30 @@
                         this.isPaused = true
                     }, 500 )
                 } )
-            } )
+			} )
+
             this.innerAudio.onSeeked( () => {
                 this.isPaused    = false
                 this.currentTime = this.innerAudio.currentTime
                 this.innerAudio.play()
             } )
-        },
-        onUnload: function( e ) {
-            this.innerAudio.destroy()//销毁这个实例
-        },
-        methods : {
-            formatSeconds( s ) {
-                let str = ''
-                if( s > 0 ) {
-                    const minutes = Math.floor( s / 60 )
-                    const seconds = Math.floor( s - minutes * 60 )
-                    let mStr      = minutes < 10 ? '0' + minutes : minutes
-                    let sStr      = seconds < 10 ? '0' + seconds : seconds
-                    str           = mStr + ':' + sStr + '"'
-                }
+		},
+		onUnload: function( e ) {
+			this.innerAudio.destroy()
+		},
+		methods : {
+			formatSeconds( s ) {
+				let str = ''
+				if( s > 0 ) {
+					const minutes = Math.floor( s / 60 )
+					const seconds = Math.floor( s - minutes * 60 )
+					let mStr      = minutes < 10 ? '0' + minutes : minutes
+					let sStr      = seconds < 10 ? '0' + seconds : seconds
+					str           = mStr + ':' + sStr + '"'
+				}
 
-                return str
-            },
+				return str
+			},
             forwardMusic( second ) {
                 let diff = this.currentTime + second
                 console.log( 'forwardMusic', this.currentTime, diff, this.innerAudio.duration )
@@ -134,28 +128,27 @@
                     this.innerAudio.seek( diff )
                 }
             },
-            pauseMuisc() {
-                this.innerAudio.pause()
-                this.currentTime = this.innerAudio.currentTime
-                this.isPaused    = true
-                this.innerAudio.offEnded()
-                this.innerAudio.offStop()
-                this.innerAudio.offTimeUpdate()
-            },
-            playRecord() {
-                this.innerAudio.src = this.url
-                if( this.isPaused ) {
-                    this.innerAudio.play()
-                    this.innerAudio.startTime = this.currentTime
-                    this.isPaused        = false
-                }else{
-                    this.pauseMuisc()
-                }
-            }
-        }
-    }
+			pauseMuisc() {
+				this.innerAudio.pause()
+				this.currentTime = this.innerAudio.currentTime
+				this.isPaused    = true
+				this.innerAudio.offEnded()
+				this.innerAudio.offStop()
+				this.innerAudio.offTimeUpdate()
+			},
+			playRecord() {
+				this.innerAudio.src = this.node.attr.src
+				if( this.isPaused ) {
+					this.innerAudio.play()
+					this.innerAudio.startTime = this.currentTime
+					this.isPaused        = false
+				}else{
+					this.pauseMuisc()
+				}
+			}
+		}
+	}
 </script>
-
 <style scoped>
     .icon {
         display: inline-block;
